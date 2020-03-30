50e972d1784bb62c9987ad936af2619c3ed28d19
alexey-plotnikov <alexey.plotnikov97@gmail.com>
2020-03-31 01:14:12 +0300

    header changes

--- vocter-frontend-task/src/components/Header/Header.js
+++ vocter-frontend-task/src/components/Header/Header.js
@@ -9,25 +9,28 @@ const Header = (props) => {
         <div className='header-wrapper'>
             <div className='header-nav-bar'>
                 <a className='header-title' href='/'>
-                    <span className='icon-header-logo'></span>
+                    <span className='header-logo-icon'></span>
                     <span className='title'>{HeaderConstants.KABOODLE}</span>
                 </a>
                 <div className='header-vertical-line'></div>
                 <div className='header-tagline'>{HeaderConstants.TAGLINE}</div>
                 <a className='header-sign-up' href='/'>
-                    <span className="icon-user"></span>
+                    <span className="user-icon"></span>
                     <span className='sign-up'> {HeaderConstants.SIGN_UP}</span>
                 </a>
                 <a className='header-log-in' href='/'>
-                    <span className="icon-user-2"></span>
+                    <span className="user-2-icon"></span>
                     <span className='log-in'> {HeaderConstants.LOG_IN}</span>
                 </a>
                 <a className='header-about' href='/'>
-                    <span className='icon-about'></span>
+                    <span className='about-icon'></span>
                     <span className='about'>{HeaderConstants.ABOUT}</span>
                 </a>
             </div>
-            <div className='header-base'></div>
+            <div className='header-base'>
+                <span className='title'>{HeaderConstants.KABOODLE}</span>
+                <span className='logo'></span>
+            </div>
         </div >
     );
 }
